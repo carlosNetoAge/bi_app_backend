612d3847bae804c45f058c296c34eeb7eebcff18
Carlos Neto <carlosn7to@gmail.com>
2022-07-14 15:34:55 -0300

    finalizado

--- routes/api.php
+++ routes/api.php
@@ -21,25 +21,25 @@ Route::post('/login', [\App\Http\Controllers\AuthenticController::class, 'login'
 
 Route::middleware(\App\Http\Middleware\VerifyToken::class)->prefix('')->group(function () {
 
-    Route::get('sub_menu/{id}', [\App\Http\Controllers\MenuSubItemsController::class, 'show_unique']);
-    Route::resource('menu_items', \App\Http\Controllers\MenuItemsController::class);
-    Route::resource('sub_menus', \App\Http\Controllers\MenuSubItemsController::class);
-    Route::post('sub_menu/iframe', [\App\Http\Controllers\MenuSubItemsController::class, 'update_iframe']);
-
-    Route::prefix('allowed')->group(function() {
-        Route::get('list_menu', [\App\Http\Controllers\MenuAllowedController::class, 'list_menu']);
-        Route::get('list_submenu', [\App\Http\Controllers\MenuAllowedController::class, 'list_subMenu']);
-        Route::get('menu_general', [\App\Http\Controllers\MenuAllowedController::class, 'menu_general']);
-        Route::get('menu_submenu_general', [\App\Http\Controllers\MenuAllowedController::class, 'menu_submenu_general']);
-    });
-
-    Route::resource('users', \App\Http\Controllers\UserController::class);
-    Route::put('users/restore/{id}', [\App\Http\Controllers\UserController::class, 'restore']);
-    Route::get('/teste', [\App\Http\Controllers\TestController::class, 'index']);
-    Route::get('/info-geral', [\App\Http\Controllers\InfoGeneralController::class, 'users_counts']);
-
-    Route::resource('menu-permissions', \App\Http\Controllers\MenuItemPermissionsController::class);
-    Route::resource('submenu-permissions', \App\Http\Controllers\MenuSubitemPermissionsController::class);
 
 });
 
+Route::get('sub_menu/{id}', [\App\Http\Controllers\MenuSubItemsController::class, 'show_unique']);
+Route::resource('menu_items', \App\Http\Controllers\MenuItemsController::class);
+Route::resource('sub_menus', \App\Http\Controllers\MenuSubItemsController::class);
+Route::post('sub_menu/iframe', [\App\Http\Controllers\MenuSubItemsController::class, 'update_iframe']);
+
+Route::prefix('allowed')->group(function() {
+    Route::get('list_menu', [\App\Http\Controllers\MenuAllowedController::class, 'list_menu']);
+    Route::get('list_submenu', [\App\Http\Controllers\MenuAllowedController::class, 'list_subMenu']);
+    Route::get('menu_general', [\App\Http\Controllers\MenuAllowedController::class, 'menu_general']);
+    Route::get('menu_submenu_general', [\App\Http\Controllers\MenuAllowedController::class, 'menu_submenu_general']);
+});
+
+Route::resource('users', \App\Http\Controllers\UserController::class);
+Route::put('users/restore/{id}', [\App\Http\Controllers\UserController::class, 'restore']);
+Route::get('/teste', [\App\Http\Controllers\TestController::class, 'index']);
+Route::get('/info-geral', [\App\Http\Controllers\InfoGeneralController::class, 'users_counts']);
+
+Route::resource('menu-permissions', \App\Http\Controllers\MenuItemPermissionsController::class);
+Route::resource('submenu-permissions', \App\Http\Controllers\MenuSubitemPermissionsController::class);
