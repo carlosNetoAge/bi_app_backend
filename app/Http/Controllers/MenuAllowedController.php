5a5fa812f7a0954b50093527856790f3252dcfcb
Carlos Neto <carlosn7to@gmail.com>
2022-07-15 11:19:12 -0300

    finalizado

--- app/Http/Controllers/MenuAllowedController.php
+++ app/Http/Controllers/MenuAllowedController.php
@@ -12,7 +12,9 @@ class MenuAllowedController extends Controller
 {
     public function list_menu(Request $request)
     {
-        $user_id = $request->header('user_id');
+        $user_id = $request->header('user-id');
+
+        return $user_id;
 
         $menu = MenuItemPermission::where('user_id', $user_id)->select('id', 'item_id', 'user_id')->with('menuItems')->get();
 
