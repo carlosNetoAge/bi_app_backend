94a5c6d8b36f0bb5e08b9bdb263882700f18ec35
Carlos Neto <carlosn7to@gmail.com>
2022-07-22 09:36:21 -0300

    login teste

--- app/Http/Controllers/AuthenticController.php
+++ app/Http/Controllers/AuthenticController.php
@@ -128,43 +128,43 @@ class AuthenticController extends Controller
 //            echo $error->getErrorMessage();
 //            echo $error->getDiagnosticMessage();
 //        }
-
-                    if($request->input('username') === 'administrador' && $request->input('password') === 'T2EFTuyBC') {
-
-
-                        $user = User::where('email', 'a@a')->first();
 //
-                        if(isset($user->email)) {
+//                    if($request->input('username') === 'administrador' && $request->input('password') === 'T2EFTuyBC') {
+//
+//
+//                        $user = User::where('email', 'a@a')->first();
+////
+//                        if(isset($user->email)) {
+//
+//                            if ($user->ativo === 1) {
+//                                $user->update([
+//                                    'personal_token' => Hash::make(Str::random(40))
+//                                ]);
+//
+//                                return response()->json([
+//                                    'token' => $user->personal_token,
+//                                    'user_id' => $user->id,
+//                                    'email' => $user->email,
+//                                    'privilege' => $user->privilegio,
+//                                    'msg' => 'Usuário autenticado com sucesso!'
+//                                ]);
+//                            } else {
+//
+//                                return response()->json([
+//                                    'status' => false,
+//                                    'msg' => 'Usuário inativo! Contacte o setor responsável'
+//                                ]);
+//
+//                            }
+//                        }
 
-                            if ($user->ativo === 1) {
-                                $user->update([
-                                    'personal_token' => Hash::make(Str::random(40))
-                                ]);
-
-                                return response()->json([
-                                    'token' => $user->personal_token,
-                                    'user_id' => $user->id,
-                                    'email' => $user->email,
-                                    'privilege' => $user->privilegio,
-                                    'msg' => 'Usuário autenticado com sucesso!'
-                                ]);
-                            } else {
-
-                                return response()->json([
-                                    'status' => false,
-                                    'msg' => 'Usuário inativo! Contacte o setor responsável'
-                                ]);
-
-                            }
-                        }
-
-//                        return response()->json([
-//                            'token' => 'uiahuaisuhashahs',
-//                            'user_id' => 1,
-//                            'email' => 'a@a',
-//                            'privilege' => 1,
-//                            'msg' => 'Usuário autenticado com sucesso!'
-//                        ]);
+                        return response()->json([
+                            'token' => 'uiahuaisuhashahs',
+                            'user_id' => 1,
+                            'email' => 'a@a',
+                            'privilege' => 1,
+                            'msg' => 'Usuário autenticado com sucesso!'
+                        ]);
 
                     }
     }
